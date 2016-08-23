e7e18539f08412d5403523a50274d5082731061a
Kate Morris <morris.kate.a@gmail.com>
2016-08-23 18:41:56 -0500

    map with directions links

--- app/assets/javascripts/location.js
+++ app/assets/javascripts/location.js
@@ -2,8 +2,14 @@ function initMap() {
 	var latLng = new google.maps.LatLng(30.109435,-93.748006);
 	var mapOpts = {
 		center: latLng,
+		scrollwheel: false,
 		zoom: 14
 	}
+
+	var strWindowFeatures = "location=yes,height=570,width=520,scrollbars=yes,status=yes";
+	var link = "https://goo.gl/maps/iiYJdqraM9v";
+	var win = "window.open('" + link + "', '_blank', 'width=800,height=520,left:200')";
+	var dirlink = '<a onclick="' + win + '">Get Directions</a>';
 
 	var map = new google.maps.Map(document.getElementById('map'), mapOpts);
 
@@ -13,7 +19,7 @@ function initMap() {
 	});
 
 	var infowindow = new google.maps.InfoWindow({
-    content: "<div><h5>Trinity Baptist Church of Orange</h5><p>1819 North 16th Street</br>Orange, TX</p></div>"
+    content: "<div><h5>Trinity Baptist Church of Orange</h5>" + dirlink + "<p>1819 North 16th Street</br>Orange, TX</p></div>"
   });
 
 	marker.setMap(map);
@@ -22,4 +28,6 @@ function initMap() {
 	marker.addListener('click', function() {
 	    infowindow.open(map, marker);
 	  });
+
+
 }
--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -1,7 +1,7 @@
 class LocationController < ApplicationController
 
 	def show
-		@address = "1819+North+16th+Street,Orange,TX"
+		@address = "1819 North 16th Street,Orange,TX"
 		@latlon = "S%7C30.109435,-93.748006"
 		@key = API_MAPS
 	end
