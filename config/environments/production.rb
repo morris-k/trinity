2906ff86c4d76f7762bebd3d412793a30e3574db
Kate Morris <morris.kate.a@gmail.com>
2017-05-31 20:13:31 -0500

    forgot to add maps api to config/environments/production ...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  API_MAPS = 'AIzaSyA3z1ThoK-JdzkawT563QrxNh7puztgt4Y'
   # Code is not reloaded between requests.
   config.cache_classes = true
 
